c93bb11ef33d373f97c839059648c4bba6750282
yehan <yehan@gct-china.com>
2024-05-09 10:54:59 +0800

    新增Django项目

--- hello.py
+++ hello.py
@@ -1,3 +1,12 @@
-while True:
-    name = input("Please enter your name:")
-    print(f"Your name is {name[0]}")
+name = input("Please enter your name:")
+print(f"Your name is {name[0]}")
+
+class Person():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def Hello(self):
+        print(f"Hello my name is {self.name}, I'm {self.age} years old.")
+
+tom = Person("Tom", 18)
+tom.Hello()
